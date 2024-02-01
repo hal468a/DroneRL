933936ca7acc1fed6913f50ba296451a66219751
hal468a <asdfg880226@gmail.com>
2024-02-01 16:21:09 +0800

    update

--- DDQN/run.py
+++ DDQN/run.py
@@ -1,16 +1,24 @@
 import os, time
 import tkinter as tk
 
-from tkinter import filedialog
 # from agent import Agent
+from tkinter import filedialog
 from ddqn_agent import DDQN_Agent
 
 def choose_env_path():
-    root = tk.Tk()
-    root.withdraw()
-    env_path = filedialog.askopenfilename()
-    print(env_path)
 
+    env_path = ""
+    file_type = [("exe file", '*.exe')]
+
+    while env_path == "":
+        root = tk.Tk()
+        root.withdraw()
+        env_path = filedialog.askopenfilename(filetypes=file_type)
+
+        if env_path == "":
+            print(f"Please select an Env file! (.exe)!!!")
+    
+    print(f"Selected Env: {env_path}")
     return env_path
 
 if __name__ == "__main__":
