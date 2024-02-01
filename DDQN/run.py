63d1b7a0e096460b38b13a12f681b3f99b5dc996
hal468a <asdfg880226@gmail.com>
2024-02-01 15:57:32 +0800

    update

--- DDQN/run.py
+++ DDQN/run.py
@@ -1,15 +1,27 @@
 import os, time
+import tkinter as tk
 
-from agent import Agent
+from tkinter import filedialog
+# from agent import Agent
 from ddqn_agent import DDQN_Agent
+
+def choose_env_path():
+    root = tk.Tk()
+    root.withdraw()
+    env_path = filedialog.askopenfilename()
+    print(env_path)
+
+    return env_path
 
 if __name__ == "__main__":
 
     # agent = Agent(useGPU=True, useDepth=True)
     # agent.train()
     
-    os.startfile("D:\\DroneWorkspace\\Enviroments\\Blocks\\Blocks.exe")
+    env_path = choose_env_path()
+    os.startfile(env_path)
     time.sleep(15)
 
     ddqn_agent = DDQN_Agent(useDepth=True)
     ddqn_agent.train()
+
