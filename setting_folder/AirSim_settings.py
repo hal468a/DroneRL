59a2b4764156724cd63ab76a1a95f2c76ca36acb
hal468a <asdfg880226@gmail.com>
2024-02-20 21:09:22 +0800

    line 20 & 23 replace user name to os.getlogin()

--- setting_folder/AirSim_settings.py
+++ setting_folder/AirSim_settings.py
@@ -1,22 +1,29 @@
+import os
 import json
 import argparse
 
 def replace_json_content(new_content):
+
     if new_content == "PX4":
-        file_path = "PX4.json"
+        file_name = "PX4.json"
     elif new_content == "Xbox":
-        file_path = "Xbox.json"
+        file_name = "Xbox.json"
     else:
-        file_path = None
+        file_name = None
         print("File Path error!!!")
 
     print(f"Readding {new_content}.json...")
-    with open(file_path, 'r') as file:
+    with open(file_name, 'r') as file:
         data = json.load(file)
     print("******************************")
 
-    print(f"Writting to C:\\Users\\Louis\\Documents\\AirSim\\settings.json...")
-    with open('C:\\Users\\Louis\\Documents\\AirSim\\settings.json', 'w') as file:
+    local_path = f"C:\\Users\\{os.getlogin()}\\Documents\\AirSim\\settings.json"
+        
+    if not os.path.exists(local_path):
+        local_path = f"C:\\Users\\{os.getlogin()}\\Onedrive\\Documents\\AirSim\\settings.json" # 如果有開啟onedrive設定檔會在這個路徑內
+    
+    print(f"Writting to {local_path}...")
+    with open(local_path, 'w') as file:
         json.dump(data, file, indent=2)
     print("******************************")
     print("Finished!")
